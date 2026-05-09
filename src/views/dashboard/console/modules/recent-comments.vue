be1856e1858efb3e52ef6f9de6058c67984916a5
lishusheng <2084447140@qq.com>
2026-05-09 23:18:12 +0800

    初始化

--- src/views/dashboard/console/modules/recent-comments.vue
+++ src/views/dashboard/console/modules/recent-comments.vue
@@ -7,26 +7,50 @@
       </div>
     </div>
 
-    <div v-if="comments.length === 0" class="flex-cc h-42 text-g-400 text-sm">
-      暂无评论
+    <div v-if="comments.length === 0" class="flex-cc flex-col h-42 text-g-400">
+      <ArtSvgIcon icon="ri:chat-3-line" class="text-3xl mb-2" />
+      <span class="text-sm">暂无评论</span>
     </div>
-    <div v-else class="space-y-4 max-h-80 overflow-y-auto">
+    <div v-else class="divide-y divide-g-200/60">
       <div
         v-for="(item, index) in comments"
         :key="index"
-        class="flex items-start p-3 rounded-lg hover:bg-g-100/50 transition-colors"
+        class="flex items-start p-3 hover:bg-g-100/50 transition-colors"
       >
-        <img :src="item.avatar" :alt="item.nickname" class="size-9 rounded-full shrink-0" />
+        <img
+          :src="item.avatar"
+          :alt="item.nickname"
+          class="size-9 rounded-full shrink-0 object-cover"
+        />
         <div class="ml-3 flex-1 min-w-0">
           <div class="flex items-center justify-between">
             <span class="text-sm font-medium text-g-800">{{ item.nickname }}</span>
             <span class="text-xs text-g-400">{{ formatTimeAgo(item.createdAt) }}</span>
           </div>
           <p class="text-xs text-g-500 mt-1 truncate">{{ item.content }}</p>
-          <p class="text-xs text-g-400 mt-0.5">
-            <template v-if="item.targetType === 1">评论了《{{ item.targetName }}》</template>
-            <template v-else-if="item.targetType === 2">评论了友链</template>
-            <template v-else>评论了说说「{{ item.targetName }}」</template>
+          <p class="text-xs text-g-400 mt-0.5 flex items-center min-w-0">
+            <ArtSvgIcon
+              :icon="
+                item.targetType === 1
+                  ? 'ri:article-line'
+                  : item.targetType === 2
+                    ? 'ri:link'
+                    : 'ri:chat-smile-2-line'
+              "
+              class="mr-1 text-sm shrink-0"
+              :class="[
+                item.targetType === 1
+                  ? 'text-blue-500'
+                  : item.targetType === 2
+                    ? 'text-green-500'
+                    : 'text-orange-500'
+              ]"
+            />
+            <span class="truncate">
+              <template v-if="item.targetType === 1">评论了《{{ item.targetName }}》</template>
+              <template v-else-if="item.targetType === 2">评论了友链</template>
+              <template v-else>评论了说说「{{ item.targetName }}」</template>
+            </span>
           </p>
         </div>
       </div>
