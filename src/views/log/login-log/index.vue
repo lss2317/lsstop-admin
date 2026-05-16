21e8e3f341e0c112afa6f577b29b92a67ac589cb
lishusheng <2084447140@qq.com>
2026-05-16 19:55:25 +0800

    初始化

---
+++ src/views/log/login-log/index.vue
@@ -0,0 +1,10 @@
+<!-- 登录日志页面 -->
+<template>
+  <div class="login-log">
+    <p>登录日志</p>
+  </div>
+</template>
+
+<script setup lang="ts">
+  defineOptions({ name: 'LoginLog' });
+</script>
---
+++ src/views/log/operation-log/index.vue
@@ -0,0 +1,10 @@
+<!-- 操作日志页面 -->
+<template>
+  <div class="operation-log">
+    <p>操作日志</p>
+  </div>
+</template>
+
+<script setup lang="ts">
+  defineOptions({ name: 'OperationLog' });
+</script>
